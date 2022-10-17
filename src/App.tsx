9066fccf8a91409a014c1a809310abe897c45a18
Stijn <stijn.vanhorenbeek@ordina.be>
2022-10-17 14:47:48 +0200

    fetch

--- src/App.tsx
+++ src/App.tsx
@@ -91,6 +91,7 @@ const useStorageState = (
 
   return [value, setValue];
 };
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
@@ -103,12 +104,15 @@ const App = () => {
 
   React.useEffect(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
-
-    getAsyncStories()
+    
+    fetch(`${API_ENDPOINT}react`)
+        // get the json of the respn
+      .then((response) => response.json())
       .then((result) => {
+        // change the property. it is now .hits
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
